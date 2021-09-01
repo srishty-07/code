f611d180d0ff90e6395e6ea681185bd0e2e3b48a
srishty <70388131+srishty-07@users.noreply.github.com>
2021-09-01 23:35:22 +0530

    01-09-2021

--- maximumElement.cpp
+++ maximumElement.cpp
@@ -1,2 +1,35 @@
 // other two unoptimised are via sorting and maping
 // using moravoting algorithm - most optimised
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int majorityElement(int a[],int n){
+    int el = a[0];//considering initial elemnt as majority elemnt
+    int count = 1;//keeping initial count as 1 
+    
+    for(int i=1;i<n;i++){
+        if(el == a[i]){
+            count++;
+        }
+        else{
+            count--;
+        }
+        if(count == 0){//matlab agar count -- hote hote zero ho jata hai tab hum dubara se agle element ko as initial element
+        // lenge aur dubara saare steps karenge
+            el = a[i];
+            count = 1;
+        }
+    }
+    
+}
+int main()
+{
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    cout<<majorityElement(a,n);
+    return 0;
+}
