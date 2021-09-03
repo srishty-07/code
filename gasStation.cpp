9063ba4a476cfec773d5214e85a19556079ed275
srishty <70388131+srishty-07@users.noreply.github.com>
2021-09-03 23:30:58 +0530

    03-09-2021
    Leetcode
    https://leetcode.com/problems/gas-station/

---
+++ gasStation.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int si = 0;
+        int curr =0;
+        int net = 0;
+        
+        for(int i=0;i<cost.length();i++){
+            net += cost[i]-gas[i];
+            curr += cost[i]-gas[i];
+            if(curr<0){
+                si = i+1;
+                curr = 0;
+            }
+        }
+        if(net < 0){
+            return -1;
+        }
+    }
+};
