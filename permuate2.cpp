2313b36dda5b632729a891eaf63ff057a642ad90
Srishty Bedi <70388131+srishty-07@users.noreply.github.com>
2021-09-12 22:23:40 +0530

    NoDuplicates___12.09.2021

---
+++ permuate2.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+void helper(vector<int> a, vector<vector<int>> &ans,int idx){
+    if(idx == a.size()){
+        ans.push_back(a);
+        return;
+    }
+    
+    for(int i=idx;i<a.size();i++){
+        if(i != idx && a[i]==a[idx]){
+            continue;
+            // bcoz agar same elemet ko hum swap karenge to fir voh duplictaes create karga isliye continue
+        }
+        swap(a[i],a[idx]);
+        helper(a,ans,idx+1);
+    }
+}
+vector<vector<int>> permute(vector<int> nums){
+    sort(nums.begin(),nums.end());
+    vector<vector<int>> ans;
+    helper(nums,ans,0);
+    return ans;
+}
+int main()
+{
+    int n;
+    cin>>n;
+    vector<int> a(n);
+    for(auto &i:a){
+        cin>>i;
+    }
+    vector<vector<int>> res = permute(a);
+    for(auto v: res){
+        for(auto i : v){
+            cout<<i<<" ";
+        }cout<<endl;
+    }
+    
+    return 0;
+}
