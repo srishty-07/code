3e1824a59f9c28b85f32291004f4c53a9f3122c8
Srishty Bedi <70388131+srishty-07@users.noreply.github.com>
2021-09-12 16:58:17 +0530

    12-09-2021

--- STLtricks.cpp
+++ STLtricks.cpp
@@ -2,36 +2,32 @@
 #include<bits/stdc++.h>
 using namespace std;
 vector<vector<int>> ans;
-void permutate(vector<int> &a,int idx){
-    // base case
-    if(idx ==  a.size()){
-        ans.push_back(a);
-        return;
-    }
-    for(int i=idx;i<a.size();i++){
-        swap(a[i],a[idx]);
-        permutate(a,idx+1);
-        swap(a[i],a[idx]);
-    }
-    return;
-}
 int main()
 {
     int n;
     cin>>n;
     vector<int> a(n);
-    for(auto &i : a){
+    for(auto &i:a){
         cin>>i;
     }
-    // stl technique
     sort(a.begin(),a.end());
     do{
         ans.push_back(a);
     }while(next_permutation(a.begin(),a.end()));
     
-    for(auto v : ans){
+    for(auto v: ans){
         for(auto i : v){
             cout<<i<<" ";
         }cout<<endl;
     }
+    
+    return 0;
 }
+// no of permutation using the inbuilt function 
+// 1 2 3
+
+// 1 3 2
+// 2 1 3
+// 2 3 1
+// 3 1 2
+// 3 2 1
