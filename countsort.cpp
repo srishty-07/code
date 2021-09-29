9c09041dadf771289a1f25b500162a9e05db384f
Srishty Bedi <70388131+srishty-07@users.noreply.github.com>
2021-09-30 00:04:56 +0530

    Update countsort.cpp

--- countsort.cpp
+++ countsort.cpp
@@ -1,3 +1,5 @@
+// 15
+// 9 6 3 5 3 4 3 9 6 4 6 5 8 9 9
 #include <iostream>
 #include<bits/stdc++.h>
 using namespace std;
@@ -44,15 +46,30 @@ int main()
     }
     
     struct Pair minmax = getMinMax(a, n);
-	int freq[minmax.max-minmax.min+1] = {0};
+    int range = minmax.max-minmax.min+1;
+	int freq[range] = {0};
 	
 	for(int i=0;i<n;i++){
 	    //hum array ko iterate karenge aur jo value ati hai suppose a[0]=9 to usme main se min ko minus karte hai aur us position pe of frequency array hum value/freq ko ++ kardete hai 
 	    int point = a[i] - minmax.min;
 	    freq[point]++;
 	}
-	for(int i=0;i<(minmax.max-minmax.min+1);i++){
-	    cout<<freq[i]<<" ";
+       //converting freq array into prefix sum array
+	for(int i=0;i < range;i++){
+	    freq[i] = freq[i]+freq[i-1];//adding the previous freq value to get the pos 
+	    //jis position tak voh no hoga us postion ko store kar lenge in prefix array
 	}
+	// we will start from backwards because we need correct order like if we have students with same marks so order will be according to the no jiska pehle aya or roll no
+    // this is stable sort	
+    int ans[n];
+    for(int i=n-1;i>=0;i--){
+        int val = a[i];
+        int pos = freq[val - minmax.min];
+        int idx = pos-1;
+        ans[idx] = val;
+    }
+    for(int i=0;i<n;i++){
+        cout<<a[i]<<" ";
+    }
 	return 0;
 }
