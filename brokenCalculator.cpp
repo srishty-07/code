d1024a0f9584112342742cfe961024de826a359d
Srishty Bedi <70388131+srishty-07@users.noreply.github.com>
2021-10-01 22:25:54 +0530

    Update brokenCalculator.cpp

--- brokenCalculator.cpp
+++ brokenCalculator.cpp
@@ -1,4 +1,5 @@
 // https://leetcode.com/problems/broken-calculator/
+// leetcode soln
 class Solution {
 public:
     int brokenCalc(int startValue, int target) {
@@ -16,3 +17,47 @@ public:
         return ans + X - Y;
     }
 };
+
+/******************************************************************************
+TC-O(log n)
+recursive approach
+input - 
+4
+2 3
+5 8
+3 10
+1024 1
+
+output-
+2
+2
+3
+1023
+*******************************************************************************/
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int brokenCalc(int startValue, int target) {
+    if(startValue>=target){
+        return startValue-target;
+    }
+    // checking if we can half the target so 
+    if(target%2==0){
+        return 1 + brokenCalc(startValue,target/2);
+        //ab baki 1/2 part ke liye recursion will take place
+    }
+    
+    return 1 + brokenCalc(startValue,target+1);
+}
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--){
+    int startValue,target;
+    cin>>startValue>>target;
+    cout<<brokenCalc(startValue,target)<<endl;
+    }
+    return 0;
+}
+
